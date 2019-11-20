1ef16519b3f2a4d3c62a889027bf5a9b34cae226
Benjamin Yu <tybung1247@gmail.com>
2019-11-19 19:43:13 -0500

    functions complete

--- app/src/main/java/com/example/birdwatchingapp/BirdSighting.java
+++ app/src/main/java/com/example/birdwatchingapp/BirdSighting.java
@@ -1,4 +1,17 @@
 package com.example.birdwatchingapp;
 
 public class BirdSighting {
+
+    public String birdName;
+    public String zipCode;
+    public String personReporting;
+
+    public BirdSighting() {
+    }
+
+    public BirdSighting(String birdName, String zipCode, String personReporting) {
+        this.birdName = birdName;
+        this.zipCode = zipCode;
+        this.personReporting = personReporting;
+    }
 }
--- app/src/main/java/com/example/birdwatchingapp/MainActivity.java
+++ app/src/main/java/com/example/birdwatchingapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.birdwatchingapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,12 +10,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button buttonCreate, buttonEdit, buttonDelete, buttonFind;
-    EditText editTextCreateTitle, editTextCreateAuthor, editTextEditTitle,
-            editTextEditAuthor;
-    EditText editTextDeleteAuthor, editTextFindAuthor;
+    Button buttonCreate, buttonFind;
+    EditText editTextCreateZip, editTextCreateBird, editTextCreatePerson;
+    EditText editTextFindZip;
     TextView textViewShowBooks;
 
     @Override
@@ -22,34 +29,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         buttonCreate = findViewById(R.id.buttonCreate);
-        buttonDelete = findViewById(R.id.buttonDelete);
-        buttonEdit = findViewById(R.id.buttonEdit);
         buttonFind = findViewById(R.id.buttonFind);
 
-        editTextCreateAuthor = findViewById(R.id.editTextCreateAuthor);
-        editTextCreateTitle = findViewById(R.id.editTextCreateTitle);
-        editTextEditAuthor = findViewById(R.id.editTextEditAuthor);
-        editTextEditTitle = findViewById(R.id.editTextEditTitle);
-        editTextDeleteAuthor = findViewById(R.id.editTextDeleteAuthor);
-        editTextFindAuthor = findViewById(R.id.editTextFindAuthor);
+        editTextCreateZip = findViewById(R.id.editTextCreateZip);
+        editTextCreateBird = findViewById(R.id.editTextCreateBird);
+        editTextCreatePerson = findViewById(R.id.editTextCreatePerson);
+        editTextFindZip = findViewById(R.id.editTextFindZip);
 
         textViewShowBooks = findViewById(R.id.textViewShowBooks);
 
         buttonCreate.setOnClickListener(this);
-        buttonEdit.setOnClickListener(this);
-        buttonDelete.setOnClickListener(this);
         buttonFind.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("bird sightings");
+
         if (view == buttonCreate) {
-        } else if (view == buttonEdit){
+            String createZip = editTextCreateZip.getText().toString();
+            String createBird = editTextCreateBird.getText().toString();
+            String createPerson = editTextCreatePerson.getText().toString();
 
-        } else if (view == buttonDelete){
+            BirdSighting createSighting = new BirdSighting(createBird, createZip, createPerson);
 
-        } else if (view == buttonFind){
+            myRef.push().setValue(createSighting); // pushes new object to database and asks for new identifier
 
+        }
+        else if (view == buttonFind){
+            String findZip = editTextFindZip.getText().toString();
+            myRef.orderByChild("zipCode").equalTo(findZip).addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    //String findKey = dataSnapshot.getKey();
+                    BirdSighting foundSighting = dataSnapshot.getValue(BirdSighting.class);
+                    String findBirdName = foundSighting.birdName;
+                    String findReporter = foundSighting.personReporting;
+                    String findZip = foundSighting.zipCode;
+
+                    textViewShowBooks.setText("The last bird sighting within the area with ZIP code " + findZip
+                    + " was reported by " + findReporter + ". The bird sighted was a " + findBirdName + "."
+                    );
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
         }
     }
 }
